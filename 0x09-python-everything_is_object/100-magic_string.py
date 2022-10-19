7790d3bd8cc21f1a19d346c8b11a3c9255dcdbc0
amjonas <amoquandohjonas@gmail.com>
2022-10-19 02:12:51 +0000

    fixed code

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     setattr(magic_string, "x", getattr(magic_string, "x", -1) + 1)
-    return "Best" + ", Best" * getattr(magic_string, "x", 0)
+    return "Best" + ", School" * getattr(magic_string, "x", 0)
